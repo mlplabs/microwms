627767cfca7e383d35d2bd50faa06b40b3bdd02d
Mike <mikelpsv@gmail.com>
2023-02-27 18:49:21 +0300

    fix install

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,9 +9,9 @@ if [ -d $BASE_DIR ]; then
   systemctl stop microwms
   sleep 5
   echo "Copyng microwms files..."
-  mv $BASE_DIR/mwms.daemon $BASE_DIR/mwms.daemon.bak
+  mv $BASE_DIR/mwms-daemon $BASE_DIR/mwms-daemon.bak
   mv $BASE_DIR/.env $BASE_DIR/.env.bak
-  cp $DEPLOY_DIR/build/mwms.daemon $BASE_DIR/mwms.daemon
+  cp $DEPLOY_DIR/build/mwms-daemon $BASE_DIR/mwms-daemon
 #  cp $DEPLOY_DIR/build/.env $BASE_DIR/.env
   echo "Starting service..."
   systemctl start microwms
@@ -19,7 +19,7 @@ else
   echo "New deploy started"
   mkdir -p $BASE_DIR
   echo "Copyng microwms files..."
-  cp $DEPLOY_DIR/build/mwms.daemon $BASE_DIR/mwms.daemon
+  cp $DEPLOY_DIR/build/mwms-daemon $BASE_DIR/mwms-daemon
 #  cp $DEPLOY_DIR/build/.env $BASE_DIR/.env
   echo "Registering and starting service"
   cp $DEPLOY_DIR/scripts/microwms.service /etc/systemd/system/microwms.service
